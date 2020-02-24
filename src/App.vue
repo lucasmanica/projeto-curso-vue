1b841fdce9193348e35b281d9c413799d97eb89b
Lucas Manica <lucasmanica4@gmail.com>
2020-02-24 19:06:19 -0300

    Third commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
     <v-app>
-        <p> Projeto do Curso </p>
+        <login-page />
     </v-app>
 </template>
 
 <script>
-
-
+//AIzaSyAeWcwBZRoXUV-B906Ay4hdSz5_LZIm028
+import LoginPage from './components/login/loginPage';
     export default {
         name: 'App',
+
+        components: { LoginPage },
     };
 </script>
---
+++ src/components/login/loginPage.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-container fluid>
+        <v-row justify="center">
+            <v-col cols="12"
+            md="3"
+            class="text-center">
+                <h6 class="headline">Login</h6>
+                <v-text-field
+                    label="Token"
+                    />
+                <v-btn depressed color="green" dark block>
+                Login
+                </v-btn>
+                <v-btn text>
+                Entrar como visitante
+                </v-btn>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+<script>
+export default {
+    name: 'LoginPage',
+};
+</script>
+<style lang="stylus" scoped>
+
+</style>
